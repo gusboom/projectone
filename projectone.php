f9d12a016667aba00c67058089d4d99d4ff0181b
gustavo a buitrago <gab23@afsconnect2.njit.edu>
2015-04-02 23:56:16 -0400

    Records table update

--- projectone.php
+++ projectone.php
@@ -1,123 +1,59 @@
 <?php
 
-    
-    
-    
-# Open the File.
+# OPEN CSV FILE
 if (($handle = fopen("hd20.csv", "r")) !== FALSE) {
-    # Set the parent multidimensional array key to 0.
-    $nn = 0;
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        # Count the total keys in the row.
-        $c = count($data);
-#        echo "<p> $c fields in line $nn: <br /></p>\n";            # Populate the multidimensional array.
-        for ($x=0;$x<$c;$x++){
-                $csvarray[$nn][$x] = $data[$x];
-            }
-        $nn++;
-    }
-    # Close the File.
+	# SET THE PARENT MULTIDIMENSIONAL ARRAY KEY TO 0.
+	$nn = 0;
+	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+		# COUNT THE TOTAL KEYS IN THE ROW
+		$c = count($data);
+		# POPULATE THE MULTIDIMENSIONAL ARRAY
+		for ($x = 0; $x < $c; $x++) {
+			$csvarray[$nn][$x] = $data[$x];
+		}
+		$nn++;
+	}
+	# CLOSE THE FILE
 	fclose($handle);
 }
 
-
-
-# NEW ARRAY $NAMEARRAY
+# NEW ARRAY $namearray All COLLEGE NAMES
 echo "COLLEGE NAME (LINKS):";
 $namearray = array();
-  for($i=1;$i<count($csvarray);$i++) {
+for ($i = 1; $i < count($csvarray); $i++) {
 	echo '<table>';
 	echo('<tr>');
-    echo('<td>' . '<a href='."WWW.GOOGLE.COM".'>'.$csvarray[$i][1] .'</a></td>');
-  	echo '</table>';  
+	echo('<td>' . '<a href=' . "projectone.php" . '>' . $csvarray[$i][1] . '</a></td>');
+	echo('</tr>');
+	echo '</table>';
 }
 
-
-
-
-#Print the contents of the multidimensional array. 
-#print_r($csvarray);
-    
-#echo "MULTIDIMENSIONAL ARRAY VALUE [1][2]:";  
-#print($csvarray[1][2]);
-
-#echo "<br><br>Number of records:";
-#echo count($csvarray);
-#echo "<br><br>";
-
-
-/*
-# Print the contents of the multidimensional array.
-echo "MULTIDIMENSIONAL ARRAY CONTENT:";
-  for($i=0;$i<count($csvarray);$i++) {
-	echo '<table>';
-	echo('<tr>');
-	for($j=0;$j<count($csvarray[$i]);$j++) {
-    	echo('<td>' . $csvarray[$i][$j] . '</td>');
-  	} 
-  	echo('</tr>');
-  	echo '</table>';  
-}
-*/
-
-
-
-/*    
-# NEW ARRAY $HEADARRAY
+# NEW ARRAY $headarray ALL HEADINGS
 $headarray = array();
 echo "<br><br>";
-echo "HEAD ARRAY CONTENT:";
-echo '<table>';
-echo ('<tr>');
-for($k=0;$k<count($csvarray[0]);$k++) {
-    	echo('<td>' . $csvarray[0][$k] . '</td>');
-    	$headarray[$k]=$csvarray[0][$k];
-  	} 
-echo ('</tr>');
+#echo "HEAD ARRAY CONTENT:";
+echo '<table border="1" cellpadding="6" cellspacing="3">';
+echo('<tr width="200px" height="50px">');
+for ($k = 0; $k < count($csvarray[0]); $k++) {
+	echo('<td width="200px" height="50px">' . $csvarray[0][$k] . '</td>');
+	$headarray[$k] = $csvarray[0][$k];
+}
+echo('</tr>');
 echo '</table>';
 
-echo "<br><br>";
-print_r($headarray);
-*/
-
-
-
-/*
-# NEW ARRAY $recordarray
+# NEW ARRAY $recordarray ALL RECORDS
 $recordarray = array();
+
+for ($i = 1; $i < count($csvarray); $i++) {
+	echo '<table border="1" cellpadding="6" cellspacing="3">';
+	echo('<tr width="200px" height="50px">');
+
+	for ($k = 0; $k < count($csvarray[$i]); $k++) {
+		echo('<td width="200px" height="50px">' . $csvarray[$i][$k] . '</td>');
+		$recordarray[$k] = $csvarray[$i][$k];
+	}
+	echo('</tr>');
+	echo '</table>';
+}
 echo "<br><br>";
-echo "ONE RECORD ARRAY CONTENT:";
-echo '<table>';
-echo ('<tr>');
-for($k=0;$k<count($csvarray[1]);$k++) {
-    	echo('<td>' . $csvarray[1][$k] . '</td>');
-    	$recordarray[$k]=$csvarray[1][$k];
-  	} 
-echo ('</tr>');
-echo '</table>';
-
-echo "<br><br>";
-print_r($recordarray);
-*/
-
-
-
-/*
-# ARRAY COMBINE ($HEADARRAY, INAMEARRAY)
-echo "<br><br>";	
-$c = array_combine($headarray, $inamearray);
-
-echo "COMBINE ARRAY CONTENT:";
-echo "<br><br>";
-print_r($c);	
-
-
-echo "<br><br>";
-echo "COMBINE ARRAY CONTENT IN A TABLE:";
-echo "<br><br>";
-echo ($c[3][5]);
-*/
-
-
-
 ?>
